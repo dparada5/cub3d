dfd0cd8f0bca9935cb3416184ffde562ca83eea1
Tobias Anselmo <tanselmo@c1r4s4.42malaga.com>
2024-12-02 19:47:11 +0100

    Change the printf %d for %f

--- src/maybe_delete.c
+++ src/maybe_delete.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   maybe_delete.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dparada <dparada@student.42.fr>            +#+  +:+       +#+        */
+/*   By: tanselmo <tanselmo@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 12:46:14 by dparada           #+#    #+#             */
-/*   Updated: 2024/11/27 15:09:46 by dparada          ###   ########.fr       */
+/*   Updated: 2024/12/02 18:53:54 by tanselmo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,5 +31,5 @@ void	change_spaces(t_cub *game)
 
 void	printf_player(t_cub *game)
 {
-	printf("x = %d y = %d view = %c\n", game->player->x, game->player->y, game->player->view);
+	printf("x = %f y = %f view = %c\n", game->player->x, game->player->y, game->player->view);
 }
