aa279704a4e109824326ac754a6cb7de02ad8105
Tobias Anselmo <tanselmo@c1r4s4.42malaga.com>
2024-12-02 19:41:22 +0100

    Changes in cub3d.h, add some functions and change structures

--- Include/cub3D.h
+++ Include/cub3D.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub3D.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dparada <dparada@student.42.fr>            +#+  +:+       +#+        */
+/*   By: tanselmo <tanselmo@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 10:03:57 by dparada           #+#    #+#             */
-/*   Updated: 2024/11/27 15:10:14 by dparada          ###   ########.fr       */
+/*   Updated: 2024/12/02 19:30:35 by tanselmo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,38 +19,55 @@
 //estructura de texturas
 //estructura de coordenadas?build
 
+# define W_WIDTH 1000
+# define W_HEIGHT 1000
+# define FOV 60.0
+# define N_PI 3.14159265359
+# define N_PI_2 1.57079632679
+
 typedef	struct	s_colors
 {
 	int	red;
+	int	green;
 	int	blue;
-	int	yellow;
 }				t_colors;
 
 typedef struct	s_player
 {
-	int 	x;
-	int		y;
+	double	x;
+	double	y;
 	char	view;
+	double	r_view;
 }			t_player;
+
+// typedef struct s_ray
+// {
+// 	double	pos_x; //posicion inicial del rayo
+// 	double	pos_y;
+// 	double	dir_x; // direccion que va a ir el rayo
+// 	double	dir_y;
+// 	// info dda
+// 	double	delta_dist_x; // distancia que debe recorrer el rayo para cruzar una celda del mapa en el eje x/y
+// 	double	delta_dist_y;
+// 	double	side_dist_x; // indica la distancia inicial del rayo desde su pos actual hasta el primer borde de la celda en x/y
+// 	double	side_dist_y;
+// 	double	step_x; // la direccion en que el rayo avanza en cada eje x/y
+// 	double	step_y;
+// 	// info de colisiion
+// 	int	map_x; // donde colisiona en x
+// 	int	map_y; // donde colisiona en y
+// 	double	dis; // distancia que recorre el rayo hasta la colision
+// 	int	side; // ver como lo hacemos, pero esta variable indica que lado impacto, si vertical u horizontal
+// }	t_ray;
 
 typedef struct s_ray
 {
-	double	pos_x; //posicion inicial del rayo
-	double	pos_y;
-	double	dir_x; // direccion que va a ir el rayo
-	double	dir_y;
-	// info dda
-	double	delta_dist_x; // distancia que debe recorrer el rayo para cruzar una celda del mapa en el eje x/y
-	double	delta_dist_y;
-	double	side_dist_x; // indica la distancia inicial del rayo desde su pos actual hasta el primer borde de la celda en x/y
-	double	side_dist_y;
-	double	step_x; // la direccion en que el rayo avanza en cada eje x/y
-	double	step_y;
-	// info de colisiion
-	int	map_x; // donde colisiona en x
-	int	map_y; // donde colisiona en y
-	double	dis; // distancia que recorre el rayo hasta la colision
-	int	side; // ver como lo hacemos, pero esta variable indica que lado impacto, si vertical u horizontal
+	double	ang;
+	double	sin;
+	double	cos;
+	double	distance;
+	double	col_x; // POSICION DE COLISION X / Y
+	double	col_y;
 }	t_ray;
 
 typedef struct s_coor
@@ -73,16 +90,30 @@ typedef struct s_coor
 
 typedef struct s_cub
 {
-	mlx_t*	mlx;
-	int		fd;
-	char	**map;
-	int		map_len;
-	int		error_flag;
-	int		start_map;
-	int		n_player;
+	mlx_t		*mlx;
+	mlx_image_t	*window_img;
+	int			fd;
+	char		**map;
+	int			map_len;
+	int			error_flag;
+	int			start_map;
+	int			n_player;
+	t_ray		*ray;
 	t_player	*player;
-	t_coor	*coor;
+	t_coor		*coor;
 }			t_cub;
+
+//---------------------------RAY CASTING------------------
+double	get_radian(int c);
+void	ray_casting(t_cub *g, t_ray *ray);
+
+//---------------------------INIT MLX GAME----------------
+void	init_mlx_game(t_cub *game);
+
+//---------------------------MOVES------------------------
+void	set_moves(mlx_key_data_t key, void *param);
+//---------------------------PUT TEXTURES-----------------
+void	put_textures(t_cub *g);
 
 //---------------------------UTILS------------------------
 void	ft_msj_error(t_cub *game, int use, char *str);
