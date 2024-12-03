72605d7794815cf08468e9082a8768510dfbc28a
Tobias Anselmo <tobianselmo04@gmail.com>
2024-12-03 20:37:58 +0100

    A lot of changes in Ray casting, some functions to calculate structure's variables, the next step is paint the textures. (NO NORMINETTE)

--- src/ray_casting.c
+++ src/ray_casting.c
@@ -13,6 +13,83 @@ double	get_radian(int c)
 		return (N_PI + N_PI_2);
 }
 
+static double	calculate_distance(t_cub *g, t_ray *ray, int map_x, int map_y)
+{
+	double	dis;
+
+	if (ray->side == 0)
+		dis = (map_x - g->player->x + (1 - ray->step_x) / 2) / ray->cos;
+	else
+		dis = (map_y - g->player->y + (1 - ray->step_y) / 2) / ray->sin;
+	return (dis);
+}
+
+static void	set_collision(t_cub *g, t_ray *ray, int map_x, int map_y)
+{
+	if (ray->side == 0) // Paredes verticales
+	{
+		ray->col_x = map_y + (1 - ray->step_y) / 2.0;
+		ray->col_y = ray->step_x * (map_x - g->player->x) / ray->cos + g->player->y;
+	}
+	else // Pared horizontal
+	{
+		ray->col_x = ray->step_x * (map_y - g->player->y) / ray->sin + g->player->x;
+		ray->col_y = map_x + (1 - ray->step_x) / 2.0;
+	}
+}
+
+static void	collision_bucle(t_cub *g, t_ray *ray)
+{
+	int	map_x;
+	int	map_y;
+
+	map_x = (int)g->player->x;
+	map_y = (int)g->player->y;
+	while (1)
+	{
+		if (ray->side_dist_x < ray->side_dist_y)
+		{
+			map_x += ray->step_x;
+			ray->side_dist_x += ray->delta_dist_x;
+			ray->side = 0;
+		}
+		else
+		{
+			map_y += ray->step_y;
+			ray->side_dist_y += ray->delta_dist_y;
+			ray->side = 1;
+		}
+		if (g->map[map_y][map_x] == '1')
+		{
+			set_collision(g, ray, map_x, map_y);
+			ray->distance = calculate_distance(g, ray, map_x, map_y);
+			break ;
+		}
+	}
+}
+
+static void	set_distance(t_cub *g, t_ray *ray)
+{
+	// Calcular para que lado va a dirigirse el rayo
+	if (ray->cos < 0)
+		ray->step_x = -1;
+	else
+		ray->step_x = 1;
+	if (ray->sin < 0)
+		ray->step_y = -1;
+	else
+		ray->step_y = 1;
+	// Calcular distancias
+	if (ray->step_x == 1) // Calcular distancia positivo en x
+		ray->side_dist_x = (floor(g->player->x) + 1 - g->player->x) * ray->delta_dist_x;
+	else // Calcular distancia negativo en x
+		ray->side_dist_x = (g->player->x - floor(g->player->x)) * ray->delta_dist_x;
+	if (ray->step_y == 1) // Calcular distancia positivo en y
+		ray->side_dist_y = (floor(g->player->y) + 1 - g->player->y) * ray->delta_dist_y;
+	else // Calcular distancia negativo en y
+		ray->side_dist_y = (g->player->y - floor(g->player->y)) * ray->delta_dist_y;
+}
+
 void	ray_casting(t_cub *g, t_ray *ray)
 {
 	int		i;
@@ -29,6 +106,10 @@ void	ray_casting(t_cub *g, t_ray *ray)
 		ray[i].ang = start - (ang * i);
 		ray[i].cos = cos(ray[i].ang);
 		ray[i].sin = sin(ray[i].ang);
+		ray[i].delta_dist_x = fabs(1 / ray[i].cos);
+		ray[i].delta_dist_y = fabs(1 / ray[i].sin);
+		set_distance(g, &ray[i]);
+		collision_bucle(g, &ray[i]);
 		i++;
 	}
 }
